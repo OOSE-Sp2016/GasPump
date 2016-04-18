1e00281c1ea7e205e6b389b66a63df0009e41c25
ktbird7 <kpatterson67@gmail.com>
2016-04-18 14:06:10 -0500

    Initial GUI
    Starting to build the GUI

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,7 +1,21 @@
+/*
+CS 6329.001 Spring 2016
+
+Grant Ledford 
+Hendra Suteja
+Joseph Fox 
+Katie Patterson 
+
+Main Controller class
+*/
+
 package controller;
 
 public class Controller {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        /* Create and show the test GUI */
+        ControllerJFrame TestFrame = new ControllerJFrame();
+        TestFrame.pack();
+        TestFrame.setVisible(true);
     }
 }
