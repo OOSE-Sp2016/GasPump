14554583fac8aa821b11fedb4754dcc73d56632d
ktbird7 <kpatterson67@gmail.com>
2016-04-28 16:12:30 -0500

    Card class code

--- src/CardServices/Card.java
+++ src/CardServices/Card.java
@@ -6,10 +6,30 @@ Hendra Suteja
 Joseph Fox 
 Katie Patterson 
 
-
+Handle card functions
 */
 package CardServices;
 
 public class Card {
+    private String number;
+    private String PINorZip;
+    private CardType type;
+
+    public Card(String numb, String pinorzip, CardType type) {
+        this.number = numb;
+        this.PINorZip = pinorzip;
+        this.type = type;
+    }
     
+    public String getNumber() {
+        return number;
+    }
+    
+    public String getPINorZip () {
+        return PINorZip;
+    }
+    
+    public CardType getType () {
+        return type;
+    }
 }
