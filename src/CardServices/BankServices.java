09b747367b4198d4d095a143e215bad2ac1304cf
ktbird7 <kpatterson67@gmail.com>
2016-04-28 16:31:50 -0500

    Bank services code

--- src/CardServices/BankServices.java
+++ src/CardServices/BankServices.java
@@ -6,10 +6,38 @@ Hendra Suteja
 Joseph Fox 
 Katie Patterson 
 
-
+Communicating with the bank
 */
 package CardServices;
 
 public class BankServices {
+    /* get card type from bank */
+    public CardType requestCardType(String number) {
+        /* for testing purposes, if first char is 'c' then credit,
+            if first char is 'd' then debit. Normally bank verification would
+            happen here
+        */
+        CardType type;
+      
+        if (number.charAt(0) == 'c') {
+            type = CardType.CREDIT;
+        } else {
+            type = CardType.DEBIT;
+        }
+        
+        return type;
+    }
     
+    public Card verifyCardAndBalance(String number, 
+            String pinorzip, CardType type) {
+        Card card = new Card(number, pinorzip, type);
+        
+        /* bank verification would happen here */
+        
+        return card;
+    }
+    
+    public void chargeCard() {
+        /* charge the card */
+    }
 }
